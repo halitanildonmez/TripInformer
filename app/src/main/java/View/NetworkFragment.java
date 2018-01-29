8910a5726a578d8d58a3216e11d1e4db3a2c9433
halitanildonmez <halitanil.donmez@gmail.com>
2018-01-29 22:11:42 +0100

    can download json, network setup, permission added, json tries

---
+++ app/src/main/java/View/NetworkFragment.java
@@ -0,0 +1,79 @@
+package View;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by anildonmez on 29.1.2018.
+ */
+
+public class NetworkFragment extends Fragment {
+    public static final String TAG = "NetworkFragment";
+    public static final String URL_KEY = "https://google.com";
+
+    private DownloadCallback mCallback;
+    private DownloadTask mDownloadTask;
+    private String mUrlString;
+
+    /**
+     * Static initializer for NetworkFragment that sets the URL of the host it will be downloading
+     * from.
+     */
+    public static NetworkFragment getInstance(FragmentManager fragmentManager, String url) {
+        NetworkFragment networkFragment = new NetworkFragment();
+        Bundle args = new Bundle();
+        args.putString(URL_KEY, url);
+        networkFragment.setArguments(args);
+        fragmentManager.beginTransaction().add(networkFragment, TAG).commit();
+        return networkFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUrlString = getArguments().getString(URL_KEY);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Host Activity will handle callbacks from task.
+        mCallback = (DownloadCallback) context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        // Clear reference to host Activity to avoid memory leak.
+        mCallback = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        // Cancel task when Fragment is destroyed.
+        cancelDownload();
+        super.onDestroy();
+    }
+
+    /**
+     * Start non-blocking execution of DownloadTask.
+     */
+    public void startDownload() {
+        cancelDownload();
+        mDownloadTask = new DownloadTask(mCallback);
+        mDownloadTask.execute(mUrlString);
+    }
+
+    /**
+     * Cancel (and interrupt if necessary) any ongoing DownloadTask execution.
+     */
+    public void cancelDownload() {
+        if (mDownloadTask != null) {
+            mDownloadTask.cancel(true);
+        }
+    }
+
+}
--- app/src/main/java/tripinformer/commute/com/tripinformer/MainActivity.java
+++ app/src/main/java/tripinformer/commute/com/tripinformer/MainActivity.java
@@ -1,17 +1,27 @@
 package tripinformer.commute.com.tripinformer;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+import View.DownloadCallback;
+import View.NetworkFragment;
+
+public class MainActivity extends FragmentActivity implements DownloadCallback {
 
     private TextView mTextMessage;
+    private boolean mDownloading = false;
+    private NetworkFragment mNetworkFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,14 +48,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mNetworkFragment = NetworkFragment.getInstance(getFragmentManager(), "https://google.com");
+        startDownload();
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        
+
         Chronometer ch = (Chronometer) findViewById(R.id.chronometer3);
         ch.setCountDown(true);
         ch.setBase(SystemClock.elapsedRealtime() + 35*60*1000);
         ch.start();
     }
 
+    private void startDownload () {
+        if (!mDownloading && mNetworkFragment != null) {
+            mNetworkFragment.startDownload();
+            mDownloading = true;
+        }
+    }
+
+    @Override
+    public void updateFromDownload(Object result) {
+        // Should updated UI
+    }
+
+    @Override
+    public NetworkInfo getActiveNetworkInfo() {
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connManager.getActiveNetworkInfo();
+    }
+
+    @Override
+    public void onProgressUpdate(int progressCode, int percentComplete) {
+        switch (progressCode) {
+            case Progress.ERROR:
+                Log.e("MyApp", "NOT CONNECFTED");
+                break;
+            case Progress.CONNECT_SUCCESS:
+                Log.d("MyApp","Connnected");
+                break;
+            case Progress.GET_INPUT_STREAM_SUCCESS:
+                break;
+            case Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
+                break;
+            case Progress.PROCESS_INPUT_STREAM_SUCCESS:
+                break;
+        }
+    }
+
+    @Override
+    public void finishDownloading() {
+        mDownloading = false;
+
+    }
 }
