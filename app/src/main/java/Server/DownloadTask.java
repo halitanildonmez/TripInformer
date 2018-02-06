cbe404c126d46afe9cb632fec309d32e9da99ac9
halitanildonmez <halitanil.donmez@gmail.com>
2018-02-06 22:10:35 +0100

    ignore file

---
+++ app/src/main/java/Server/DownloadTask.java
@@ -0,0 +1,181 @@
+package Server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import tripinformer.commute.com.tripinformer.AppConstants;
+
+/**
+ * Created by anildonmez on 29.1.2018.
+ */
+
+public class DownloadTask extends android.os.AsyncTask <String, Integer, DownloadTask.Result> {
+
+    private DownloadCallback<String> mCallback;
+    private static final String URL_ADDRESS = "";
+
+    DownloadTask(DownloadCallback<String> callback) {
+        setCallback(callback);
+    }
+
+    void setCallback(DownloadCallback<String> callback) {
+        mCallback = callback;
+    }
+
+    static class Result {
+        public String mResultValue;
+        public Exception mException;
+        public Result(String resultValue) {
+            mResultValue = resultValue;
+        }
+        public Result(Exception exception) {
+            mException = exception;
+        }
+    }
+
+    /**
+     * Cancel background network operation if we do not have network connectivity.
+     */
+    @Override
+    protected void onPreExecute() {
+        if (mCallback != null) {
+            NetworkInfo networkInfo = mCallback.getActiveNetworkInfo();
+            if (networkInfo == null || !networkInfo.isConnected() ||
+                    (networkInfo.getType() != ConnectivityManager.TYPE_WIFI
+                            && networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)) {
+                // If no connectivity, cancel task and update Callback with null data.
+                mCallback.updateFromDownload(null);
+                cancel(true);
+            }
+
+        }
+    }
+
+    /**
+     * Defines work to perform on the background thread.
+     */
+    @Override
+    protected DownloadTask.Result doInBackground(String... urls) {
+        Result result = null;
+        if (!isCancelled() && urls != null && urls.length > 0) {
+            String urlString = urls[0];
+            try {
+                URL url = new URL(AppConstants.CONNECTION_URL);
+                String resultString = downloadUrl(url);
+                if (resultString != null) {
+                    result = new Result(resultString);
+                } else {
+                    throw new IOException("No response received.");
+                }
+            } catch(Exception e) {
+                result = new Result(e);
+            }
+        }
+        return result;
+    }
+
+    private String downloadUrl(URL url) throws IOException {
+        InputStream stream = null;
+        HttpURLConnection connection = null;
+        String result = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            // Timeout for reading InputStream arbitrarily set to 3000ms.
+            connection.setReadTimeout(3000);
+            // Timeout for connection.connect() arbitrarily set to 3000ms.
+            connection.setConnectTimeout(3000);
+            // For this use case, set HTTP method to GET.
+            connection.setRequestMethod("GET");
+            // Already true by default but setting just in case; needs to be true since this request
+            // is carrying an input (response) body.
+            connection.setDoInput(true);
+            // Open communications link (network traffic occurs here).
+            connection.connect();
+            publishProgress(DownloadCallback.Progress.CONNECT_SUCCESS);
+            int responseCode = connection.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                throw new IOException("HTTP error code: " + responseCode);
+            }
+            // Retrieve the response body as an InputStream.
+            stream = connection.getInputStream();
+            publishProgress(DownloadCallback.Progress.GET_INPUT_STREAM_SUCCESS, 0);
+            if (stream != null) {
+                // Converts Stream to String with max length of 10000.
+                result = readStream(stream, 10000);
+                JSONObject jsonObject = new JSONObject(result);
+                if (jsonObject.getInt("StatusCode") == 0) {
+                    Log.d("MY APP", "JSON IS DONE BITCHES");
+                    JSONObject responseData = jsonObject.getJSONObject("ResponseData");
+                    JSONArray metros = responseData.getJSONArray("Metros");
+                    if (metros != null) {
+                        Log.d("MY APP ", "NOT NULL");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close Stream and disconnect HTTPS connection.
+            if (stream != null) {
+                stream.close();
+            }
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Converts the contents of an InputStream to a String.
+     */
+    public String readStream(InputStream stream, int maxReadSize)
+            throws IOException {
+        Reader reader = new InputStreamReader(stream, "UTF-8");
+        char[] rawBuffer = new char[maxReadSize];
+        int readSize;
+        StringBuffer buffer = new StringBuffer();
+        while (((readSize = reader.read(rawBuffer)) != -1) ) {
+            if (readSize > maxReadSize) {
+                readSize = maxReadSize;
+            }
+            buffer.append(rawBuffer, 0, readSize);
+            maxReadSize -= readSize;
+        }
+        Log.d("MyApp", "JSON PARSE " + buffer.toString());
+        return buffer.toString();
+    }
+
+    /**
+     * Updates the DownloadCallback with the result.
+     */
+    @Override
+    protected void onPostExecute(Result result) {
+        if (result != null && mCallback != null) {
+            if (result.mException != null) {
+                mCallback.updateFromDownload(result.mException.getMessage());
+            } else if (result.mResultValue != null) {
+                mCallback.updateFromDownload(result.mResultValue);
+            }
+            mCallback.finishDownloading();
+        }
+    }
+
+    /**
+     * Override to add special behavior for cancelled AsyncTask.
+     */
+    @Override
+    protected void onCancelled(Result result) {
+    }
+}
